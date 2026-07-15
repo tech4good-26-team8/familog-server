56739f1d91d27f776351f047230607f9209c3d87
wngktjd13 <seonguk3553@kookmin.ac.kr>
2026-07-16 01:30:44 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/familog/server/FamilogServerApplication.java
@@ -0,0 +1,13 @@
+package com.familog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/familog/server/FamilogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familog.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
